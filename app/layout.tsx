d0fbb2e337d62f95ec7a4fad63ca3a9b83889248
Yasu1025 <sillyjp25@gmail.com>
2023-11-19 09:48:31 +0900

    [Fature] Layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { Inter } from 'next/font/google'
 import Head from './head'
 import { GlobalProvider } from './GlobalProvider'
 import Script from 'next/script'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +15,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='en'>
       <Head />
       <body className={inter.className}>
-        <GlobalProvider>{children}</GlobalProvider>
+        <GlobalProvider>
+          <Header />
+          {children}
+          <Footer />
+        </GlobalProvider>
         <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'></Script>
       </body>
     </html>
---
+++ components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-1 pt-5'>
+      <p className='text-center mt-1'>Hotel Booking - 20xx-20xx, All Rights Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
