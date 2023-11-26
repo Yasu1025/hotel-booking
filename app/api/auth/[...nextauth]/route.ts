7b823fcc66ec7f6c6f39cce6e5cb17ba8984c50f
Yasu1025 <sillyjp25@gmail.com>
2023-11-26 13:16:17 +0900

    [Feature] auth api

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,64 @@
+import { bcrypt } from 'bcryptjs'
+import dbConnect from '@/backend/config/dbConnect'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import NextAuth from 'next-auth/next'
+import { NextRequest, NextResponse } from 'next/server'
+import User, { IUser } from '@/backend/models/user'
+
+type Credentials = {
+  email: string
+  password: string
+}
+
+type Token = {
+  user: IUser
+}
+
+async function auth(req: NextRequest, res: any) {
+  return await NextAuth(req, res, {
+    session: {
+      strategy: 'jwt',
+    },
+    providers: [
+      CredentialsProvider({
+        // @ts-ignore
+        async authorize(credentials: Credentials) {
+          dbConnect()
+          const { email, password } = credentials
+          const user = await User.findOne({ email }).select('+password')
+          if (!user) {
+            throw new Error('Invalid email or password')
+          }
+
+          const isPasswordMatched = await bcrypt.compare(password, user.password)
+
+          if (!isPasswordMatched) {
+            throw new Error('Invalid email or password')
+          }
+
+          return user
+        },
+      }),
+    ],
+    callbacks: {
+      jwt: async ({ token, user }) => {
+        console.log(token, user)
+
+        user && (token.user = user)
+
+        // TODO: update session when user is updated
+
+        return token
+      },
+      session: async ({ session, token }) => {
+        session.user = token.user as IUser
+
+        console.log('Session -> ', session)
+        return session
+      },
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+  })
+}
+
+export { auth as GET, auth as POST }
--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     API_BASE: 'http://localhost:3000',
     DB_LOCAL_URI: process.env.DB_LOCAL_URI,
     DB_URI: process.env.DB_URI,
+    // Next-Auth
+    NEXTAUTH_URL: 'http://localhost:3000',
+    NEXTAUTH_SECRET: 'FDRESCEWNFIEFJDAIFJEQOFN', // random
   },
   images: {
     domains: ['res.cloudinary.com'],
